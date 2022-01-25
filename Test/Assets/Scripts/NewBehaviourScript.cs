2e76119876f2cdf9d2afee7ece3f51c77b162977
MisterPark <40124101+MisterPark@users.noreply.github.com>
2022-01-26 00:11:31 +0900

    change
    change

---
+++ Test/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Sprite sprite;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
