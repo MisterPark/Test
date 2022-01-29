de048e680c25663ad6ad7d53c3262c053336a845
MisterPark <40124101+MisterPark@users.noreply.github.com>
2022-01-29 15:38:47 +0900

    Add Sample
    Add Sample

---
+++ Test/Assets/Scripts/Sample/SingletonSample.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonSample : MonoBehaviour
+{
+    public static SingletonSample Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
