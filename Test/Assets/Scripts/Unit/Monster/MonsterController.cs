b7568a4e3429ec628aba07dabf7a28695211075b
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-02-10 14:10:57 +0900

    몬스터샘플테스트
    monster

--- Test/Assets/Scripts/Unit/Monster/MonsterController.cs
+++ Test/Assets/Scripts/Unit/Monster/MonsterController.cs
@@ -9,7 +9,7 @@ public class MonsterController : UnitController
     Animator animator;
     public float SearchDistance;
     public float ApproachDistance;
-    public float Range;
+    public float AttackRange;
     [SerializeField] bool isSearch;
     Vector3 direction;
     float distance;
@@ -33,9 +33,14 @@ public class MonsterController : UnitController
             stat.Hp = stat.MaxHp;
         }
         if (!isSearch)
+        {
             Search();
+        }
         else
+        {
             Move();
+            
+        }
     }
 
     protected void Search()
@@ -66,14 +71,33 @@ public class MonsterController : UnitController
         }
         else
         {
+            //»ç°Å¸®
             Attack();
             stat.MoveSpeed = 0;
             animator.SetFloat("Velocity", stat.MoveSpeed);
         }
     }
 
-    protected void Attack()
+    protected bool Attack()
     {
+        DirectedToPlayer();
+        if (animator.GetInteger("Attack")==-1)
+        { 
+            animator.SetInteger("Attack", 0);
+            Vector3 tempPos = gameObject.transform.position + (gameObject.transform.forward * AttackRange);
+            tempPos.y += 0.7f;
+            DamageObjectController.Create_DamageObject(UnitStat.Team.Enemy, tempPos, 0.5f, 1.2f, 15f);
+        }
+        return false;
+    }
+
+    protected void DirectedToPlayer()
+    {
+        
+        direction = player.transform.position - transform.position;
+        direction.Normalize();
+        direction.y = 0;
+        transform.forward = direction;
         
     }
 }
